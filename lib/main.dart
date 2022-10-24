88b6f50d627295adf63510d66e9cc2abf236b53d
M7MMAD_OMAR <muhammadaaa727@gmail.com>
2022-10-24 20:53:38 +0300

    Edit files

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 
 import 'package:flutter/material.dart';
 
-void main() => (MyApp());
+void main() => MyApp();
 
 
 class MyApp extends StatelessWidget {
@@ -10,7 +10,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Main Screen"),
+      ),
+      body: Container(
+        child:
+          Text("data")
+      )
     );
   }
 }

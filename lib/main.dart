5efc42ba420a61fadeaaa9d95393a418017a993e
M7MMAD_OMAR <muhammadaaa727@gmail.com>
2022-10-25 18:26:57 +0300

    removing files

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MyHomePage();
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("hi"),
+    );
+  }
+}
+

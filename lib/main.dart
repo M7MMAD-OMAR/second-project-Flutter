9e6ff40ffc023a83ec4f237b36fc799a2f889265
M7MMAD_OMAR <muhammadaaa727@gmail.com>
2022-10-25 15:36:49 +0300

    remove tabs, then added bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -26,9 +26,44 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  late double _valueSlider = 0.0;
 
   @override
   Widget build(BuildContext context) {
-    return
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("${_valueSlider.round()}"),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [BoxShadow(
+                  color: Colors.green,
+                  blurRadius: 9,
+                  offset: Offset(2, 4)
+                ),]
+              ),
+              transform: Matrix4.rotationX(_valueSlider)..translate(20, 20, 20),
+              margin: EdgeInsets.only(top: 90, bottom: 90),
+              width: 50,
+              height: 50,
+            ),
+            Slider(onChanged: (double value) => setState( ()=> _valueSlider = value),
+              value: _valueSlider,
+              label: "$_valueSlider",
+              min: 0,
+              max: 10,
+              activeColor: Colors.red,
+              thumbColor: Colors.green,
+              divisions: 100,
+            ),
+          ],
+        ),
+      )
+    );
   }
 }

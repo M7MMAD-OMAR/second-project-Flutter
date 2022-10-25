66c627f983e2725813577a67ea93bfeb42e9aff7
M7MMAD_OMAR <muhammadaaa727@gmail.com>
2022-10-25 20:24:38 +0300

    add file

---
+++ lib/answer.dart
@@ -0,0 +1,25 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  late String titleAnswer;
+  late Function() funAnswer;
+  Answer({Key? key, required this.titleAnswer, required this.funAnswer}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(5),
+      padding: EdgeInsets.all(5),
+      child: ElevatedButton(
+        onPressed: funAnswer,
+        child: Text(titleAnswer),
+      ),
+    );
+  }
+}
+
+

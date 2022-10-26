d5c82647e917b2ebf5ea58b9f4d739b776a81738
M7MMAD_OMAR <muhammadaaa727@gmail.com>
2022-10-26 17:10:55 +0300

    finish the app answer

--- lib/main.dart
+++ lib/main.dart
@@ -23,39 +23,8 @@ class _MyAppState extends State<MyApp> {
       home: Scaffold(
         appBar: AppBar(
           title: const Text("AppBar Testing"),
-          actions: [
-            Switch(
-              value: isSwitch,
-              onChanged: (value) {
-                setState(() {
-                  isSwitch = value;
-                  if (isSwitch) {
-                    black = Colors.white;
-                    white = Colors.black;
-                  }
-                  if (!isSwitch) {
-                    black = Colors.black;
-                    white = Colors.white;
-                  }
-                });
-              },
-              // thumbColor: MaterialStateProperty.all(Colors.purple),
-              activeColor: Colors.green,
-              inactiveThumbColor: Colors.black,
-              inactiveTrackColor: Colors.white,
-            ),
-          ],
         ),
-        body: Container(
-          color: white,
-          child: indexQuestion < questionsAndAnswers.length
-              ? Quiz(
-            questionAndAnswer: questionsAndAnswers,
-            indexQuestion: indexQuestion,
-            tempAnswer: tempAnswer,
-          )
-              : Reset(funReset: resetApp, score: totalScore),
-        ),
+        body: ,
       ),
     );
   }

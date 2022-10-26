0b0ba0be5390841e1f772e0edad896034af3e8cd
M7MMAD_OMAR <muhammadaaa727@gmail.com>
2022-10-26 17:38:26 +0300

    add data from api

--- lib/api.dart
+++ lib/api.dart
@@ -1,13 +1,30 @@
-void main() {
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
+
+void main() async{
+   var temp = await fetchData();
+
+   for(var item in temp) {
+     print("User ID: ${item['userId']}");
+     print("ID: ${item['id']}");
+     print(item['title']);
+     print(item['body']);
+     print("---------------------------------------");
+   }
 }
 
 
 fetchData() async {
-  final url = "https://jsonplaceholder.typicode.com/posts";
+  const url = "https://jsonplaceholder.typicode.com/posts";
 
-  final resultDate = Uri.http(url, url);
+  final resultDate = await http.get(Uri.parse(url));
 
-  if( resultDate.)
+  if(resultDate.statusCode == 200) {
+     var obj = json.decode(resultDate.body);
+     return obj;
+  }else {
+    print("Not Good");
+  }
 
 }
